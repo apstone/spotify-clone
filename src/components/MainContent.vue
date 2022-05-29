9ef934a9bc9d14c8545b769686ce80efbaf8fe1c
Aaron Stone <apstone@protonmail.com>
2022-05-28 22:33:43 -0400

    feat: recents

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -4,19 +4,33 @@
       <div class="navigation"></div>
       <div class="profile"></div>
     </div>
-
     <div class="recent">
       <h2>Good evening</h2>
+      <div class="recent-container">
+        <div
+          v-for="(recent, index) in links.recents"
+          :key="index"
+          class="recent-tile"
+        >
+          <img class="art" :src="recent.image" />
+          <span class="title">{{ recent.title }}</span>
+          <div class="play-button"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { links } from "../data.js";
+
 export default {
   name: "MainContent",
-
   data() {
     return {};
+  },
+  computed: {
+    links: () => links,
   },
 
   mounted() {},
@@ -31,7 +45,7 @@ export default {
   height: calc(100vh - 90px);
   overflow: hidden;
   position: relative;
-  padding: 32px;
+
   .navigation {
     background-color: rgb(0, 0, 0, 0);
     position: absolute;
@@ -40,14 +54,44 @@ export default {
   }
 
   .recent {
+    background-color: rgb(160, 192, 200);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.6) 0, #121212 100%);
     text-align: left;
-    margin-top: 60px;
-
+    height: 300px;
+    padding: 90px 32px 32px 32px;
     h2 {
       color: #fff;
       font-weight: bold;
       font-size: 32px;
       line-height: 36px;
+      margin-bottom: 20px;
+    }
+
+    .recent-container {
+      display: grid;
+      grid-gap: 16px 24px;
+      grid-template: auto/repeat(auto-fill, minmax(max(270px, 25%), 1fr));
+
+      .recent-tile {
+        display: flex;
+        background-color: rgba(255, 255, 255, 0.1);
+        border-radius: 5px;
+        align-items: center;
+        .art {
+          height: 80px;
+          width: 80px;
+          border-radius: 4px 0 0 4px;
+        }
+        .title {
+          padding: 0 16px;
+          font-weight: bold;
+          color: #fff;
+        }
+        .play-button {
+          width: 50px;
+          height: 50px;
+        }
+      }
     }
   }
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,30 @@
 export const links = {
   main: ["Home", "Search", "Your library"],
   playlists: ["Favorites", "Gaming Mix", "Summer", "Vibes"],
+  recents: [
+    {
+      title: "EDM",
+      image: "../src/assets/edm-bg.jpg",
+    },
+    {
+      title: "#1825 - Ali Siddiq",
+      image: "../src/assets/jre-podcast.jpg",
+    },
+    {
+      title: "21 Savage",
+      image: "../src/assets/21-savage.jpg",
+    },
+    {
+      title: "Come home The Kids Miss You",
+      image: "../src/assets/jack-harlow.jpg",
+    },
+    {
+      title: "The Feels - Benny Benassi Remix",
+      image: "../src/assets/the-feels.jpg",
+    },
+    {
+      title: "#289 - Stephen Kotkin: Putin",
+      image: "../src/assets/lex-podcast.jpg",
+    },
+  ],
 };
